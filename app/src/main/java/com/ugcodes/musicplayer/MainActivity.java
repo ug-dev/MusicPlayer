72a943ed904547e61319c028964cc511b9d250cc
ug <gadhavanaumang007@gmail.com>
2020-08-27 20:45:56 +0530

    MusicPlayer v1.0.0

--- app/src/main/java/com/ugcodes/musicplayer/MainActivity.java
+++ app/src/main/java/com/ugcodes/musicplayer/MainActivity.java
@@ -1,10 +1,18 @@
 package com.ugcodes.musicplayer;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,10 +21,38 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-//            getWindow().setStatusBarColor(Color.TRANSPARENT);
-//        }
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
 
-
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_layout,
+                new FragmentHome()).commit();
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectedFragment = null;
+
+            switch (item.getItemId()) {
+                case R.id.nav_home:
+                    selectedFragment = new FragmentHome();
+                    break;
+                case R.id.nav_search:
+                    selectedFragment = new FragmentSearch();
+                    break;
+                case R.id.nav_stream:
+                    selectedFragment = new FragmentStream();
+                    break;
+                case R.id.nav_library:
+                    selectedFragment = new FragmentLibrary();
+                    break;
+            }
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_layout,
+                    Objects.requireNonNull(selectedFragment)).commit();
+
+            return true;
+        }
+    };
 }
