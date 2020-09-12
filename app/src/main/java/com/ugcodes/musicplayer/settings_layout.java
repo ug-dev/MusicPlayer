7158a5b5ce2ad1f9ab9e754d3b59a67b4e64902c
ug <gadhavanaumang007@gmail.com>
2020-09-12 15:29:14 +0530

    MusicPlayer v1.0.0

--- app/src/main/java/com/ugcodes/musicplayer/settings_layout.java
+++ app/src/main/java/com/ugcodes/musicplayer/settings_layout.java
@@ -7,13 +7,29 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import java.util.Objects;
 
 public class settings_layout extends Fragment {
+    private ImageView backButton;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings_layout, container, false);
+        View view = inflater.inflate(R.layout.fragment_settings_layout, container, false);
+
+        backButton = view.findViewById(R.id.settings_back_button);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Objects.requireNonNull(getFragmentManager())
+                        .beginTransaction().replace(R.id.fragment_layout,
+                        new FragmentHome()).commit();
+            }
+        });
+
+        return view;
     }
 }
