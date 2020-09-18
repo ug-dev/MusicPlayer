bfd6c75daf21e3f29f7ec65c4c991086afacc4fa
ug <gadhavanaumang007@gmail.com>
2020-09-18 20:01:02 +0530

    MusicPlayer v1.0.0

--- app/src/main/java/com/ugcodes/musicplayer/PlayerScreen.java
+++ app/src/main/java/com/ugcodes/musicplayer/PlayerScreen.java
@@ -1,15 +1,40 @@
 package com.ugcodes.musicplayer;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.content.res.AppCompatResources;
+import androidx.cardview.widget.CardView;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.graphics.drawable.DrawableCompat;
+import androidx.palette.graphics.Palette;
 
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.GradientDrawable;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.View;
+import android.view.WindowMetrics;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Objects;
+
 public class PlayerScreen extends AppCompatActivity {
+    private TextView songTitle, songArtist;
     private ImageView downPlayerScreen, menuPlayerScreen;
     private ImageView pauseButton, playButton, likeIcon, likedIcon;
+    private ImageView songPoster;
+    private CardView posterBackground;
+    private ConstraintLayout layout;
+
+    private Palette.Swatch darkVibrantSwatch;
+    private Palette.Swatch mutedSwatch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +47,38 @@ public class PlayerScreen extends AppCompatActivity {
         pauseButton = findViewById(R.id.pl_screen_pause_button);
         likeIcon = findViewById(R.id.pl_screen_like_icon);
         likedIcon = findViewById(R.id.pl_screen_liked_icon);
+        songTitle = findViewById(R.id.pl_screen_song_title);
+        songArtist = findViewById(R.id.pl_screen_song_artist);
+        posterBackground = findViewById(R.id.pl_screen_song_poster_background);
+        layout = findViewById(R.id.pl_screen_layout);
+        songPoster = findViewById(R.id.pl_screen_song_poster);
+
+        songTitle.setSelected(true);
+        songArtist.setSelected(true);
+
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        int w = displayMetrics.widthPixels;
+
+        ConstraintLayout.LayoutParams layoutParams = (ConstraintLayout.LayoutParams)
+                posterBackground.getLayoutParams();
+        layoutParams.height = (w * 85) / 100;
+        layoutParams.width = (w * 90) / 100;
+        posterBackground.setLayoutParams(layoutParams);
+
+        //Setting Gradient Background
+        Bitmap bitmap = ((BitmapDrawable) songPoster.getDrawable()).getBitmap();
+
+        Palette.from(bitmap).maximumColorCount(128).generate(new Palette.PaletteAsyncListener() {
+            @Override
+            public void onGenerated(@Nullable Palette palette) {
+                if (palette != null) {
+                    darkVibrantSwatch = palette.getDarkVibrantSwatch();
+                    mutedSwatch = palette.getMutedSwatch();
+                }
+                generateBackgroundGradient();
+            }
+        });
         
         downPlayerScreen.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -64,6 +121,26 @@ public class PlayerScreen extends AppCompatActivity {
                 Toast.makeText(PlayerScreen.this, "Menu", Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    private void generateBackgroundGradient() {
+        GradientDrawable drawable;
+
+        if (mutedSwatch != null) {
+            drawable = new GradientDrawable(
+                    GradientDrawable.Orientation.TOP_BOTTOM,
+                    new int[] {mutedSwatch.getRgb(), 0xFF111111});
+
+            layout.setBackground(drawable);
+        } else if (darkVibrantSwatch != null) {
+            drawable = new GradientDrawable(
+                    GradientDrawable.Orientation.TOP_BOTTOM,
+                    new int[] {darkVibrantSwatch.getRgb(), 0xFF111111});
+
+            layout.setBackground(drawable);
+        } else {
+            layout.setBackgroundColor(getResources().getColor(R.color.background1));
+        }
     }
 
     private void togglePlayerButton(ImageView image1, ImageView image2) {
