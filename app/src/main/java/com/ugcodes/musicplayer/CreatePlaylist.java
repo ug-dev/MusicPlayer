3fd923ed5b8dc1439fffda95e986749498ab9310
ug <gadhavanaumang007@gmail.com>
2020-09-27 22:56:05 +0530

    MusicPlayer v1.0.0

--- app/src/main/java/com/ugcodes/musicplayer/CreatePlaylist.java
+++ app/src/main/java/com/ugcodes/musicplayer/CreatePlaylist.java
@@ -1,11 +1,14 @@
 package com.ugcodes.musicplayer;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Resources;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -22,17 +25,21 @@ import android.widget.TextView;
 import java.util.Objects;
 
 public class CreatePlaylist extends AppCompatActivity {
+    private int width;
     private CardView imageBackground;
     private ImageView createIcon, createImage;
     private EditText name;
     private Button cancelButton, createButton;
+
+    private static final int PICK_IMAGE_REQUEST = 1;
+    private Uri mImageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_playlist);
 
-        int width = Resources.getSystem().getDisplayMetrics().widthPixels;
+        width = Resources.getSystem().getDisplayMetrics().widthPixels;
 
         imageBackground = findViewById(R.id.create_playlist_background);
         createIcon = findViewById(R.id.create_playlist_icon);
@@ -41,11 +48,7 @@ public class CreatePlaylist extends AppCompatActivity {
         cancelButton = findViewById(R.id.create_playlist_cancel_button);
         createButton = findViewById(R.id.create_playlist_create_button);
 
-        ConstraintLayout.LayoutParams layoutParams = (ConstraintLayout.LayoutParams)
-                imageBackground.getLayoutParams();
-        layoutParams.height = (width * 60) / 100;
-        layoutParams.width = (width * 60) / 100;
-        imageBackground.setLayoutParams(layoutParams);
+        setImageBackgroundWidth();
 
         cancelButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -60,5 +63,46 @@ public class CreatePlaylist extends AppCompatActivity {
                 onBackPressed();
             }
         });
+
+        imageBackground.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                hideSoftKeyboard(name);
+                openFileChooser();
+            }
+        });
+    }
+
+    private void setImageBackgroundWidth() {
+        ConstraintLayout.LayoutParams layoutParams = (ConstraintLayout.LayoutParams)
+                imageBackground.getLayoutParams();
+        layoutParams.height = (width * 60) / 100;
+        layoutParams.width = (width * 60) / 100;
+        imageBackground.setLayoutParams(layoutParams);
+    }
+
+    private void openFileChooser() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, PICK_IMAGE_REQUEST);
+    }
+
+    private void hideSoftKeyboard(View v) {
+        InputMethodManager imm = (InputMethodManager)
+                Objects.requireNonNull(getSystemService(Context.INPUT_METHOD_SERVICE));
+        imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (data != null) {
+            mImageUri = data.getData();
+            createImage.setImageURI(mImageUri);
+            createIcon.setVisibility(View.GONE);
+            createImage.setVisibility(View.VISIBLE);
+        }
     }
 }
